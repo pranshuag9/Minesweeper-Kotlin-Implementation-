9883d447b5d38e4c60f3a4df4373c5bc1b9d7f51
Vasudev <vasudev.w13@gmail.com>
2018-03-14 14:27:24 +0530

    Splash Finalised

--- app/src/main/java/com/example/vasudev/minesweeper/SplashActivity/SplashActivity.kt
+++ app/src/main/java/com/example/vasudev/minesweeper/SplashActivity/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.vasudev.minesweeper.SplashActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.vasudev.minesweeper.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -11,5 +12,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         splashAnimationView.playAnimation()
+        atTextView.visibility= View.VISIBLE
+        logoImageView.visibility=View.VISIBLE
+        quoteTextView.visibility=View.VISIBLE
+
     }
 }
